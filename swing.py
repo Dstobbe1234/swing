5203e05f742173ecee82429a80bdec56f563397b
David Stobbe <d.stobbe1@share.epsb.ca>
2023-10-23 13:26:16 -0600

    bad

--- swing.py
+++ swing.py
@@ -8,42 +8,52 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() /2)
-velocity = [0, 0]
-acceleration = [0, 0]
-gravity = 1
+bobAngle = (math.pi) * 3/4
 mouse = False
 
-class pendulum:
-    def __init__(self, pos):
-        self.pos = pos
-    def getAngle(self):
-        angle = math.atan2(player_pos[1]- self.pos[1], player_pos[0] - self.pos[0])
-        return angle
+def swing(mousePos, radius, angle, bobSpeed):
+    mouse = True
+    while mouse:
+        screen.fill("purple")
+        pygame.draw.line(screen, 'black', mousePos, player_pos, 1)
+        player_pos[0] = mousePos[0] + radius * math.cos(angle)
+        player_pos[1] = mousePos[1] + radius * math.sin(angle)
+        bobAcceleration = 0.0001 * math.cos(angle)
+        bobSpeed += bobAcceleration
+        # angle += math.atan(bobSpeed)
+        pygame.draw.circle(screen, "red", player_pos, 40)
+        pygame.display.flip()
+        ev = pygame.event.get()
+        for event in ev:
+            if(event.type == pygame.MOUSEBUTTONUP):
+                mouse = False
+    fall([bobSpeed * math.cos(angle), bobSpeed * math.sin(angle)])
+    
+
+
+def fall(bobSpeedVectors):
+    mouse = False
+    while not mouse:
+        screen.fill("purple")
+        bobAcceleration = 0.001
+        print(bobSpeedVectors)
+        bobSpeedVectors[1] += bobAcceleration
+        global player_pos
+        player_pos[0] += bobSpeedVectors[0]
+        player_pos[1] += bobSpeedVectors[1]
+        pygame.draw.circle(screen, "red", player_pos, 40)
+        pygame.display.flip()
+        ev = pygame.event.get()
+        for event in ev:
+            if(event.type == pygame.MOUSEBUTTONDOWN):
+                mousePos = pygame.mouse.get_pos()
+                radius = math.sqrt(((player_pos[1]-mousePos[1])**2) + ((player_pos[0]-mousePos[0])**2))
+                mouse = True
+    swing(mousePos, radius, math.atan2(player_pos[1]- mousePos[1], player_pos[0] - mousePos[0]), math.sqrt((bobSpeedVectors[0] ** 2) + (bobSpeedVectors[1] ** 2)))
 
 while running:
-    ev = pygame.event.get()
-    for event in ev:
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            radius = math.sqrt(((player_pos[1]-pos[1])**2) + ((player_pos[0]-pos[0])**2))
-            bobAngle = math.atan2(player_pos[1]- pos[1], player_pos[0] - pos[0])
-            bobSpeed = 0.0001
-            airRes = 0
-            mouse = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouse = False
-    screen.fill("purple")
-    
-    if(mouse):
-        pygame.draw.line(screen, 'black', pos, player_pos, 1)
-        player_pos[0] = pos[0] + radius * math.cos(bobAngle)
-        player_pos[1] = pos[1] + radius * math.sin(bobAngle)
-        bobAcceleration = 0.0001 * math.cos(bobAngle)
-        bobSpeed += bobAcceleration
-        bobAngle += bobSpeed
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.display.flip()
-    # clock.tick(60) 
+    fall([0, 0])
+    clock.tick(10) 
 
 pygame.quit()
         
