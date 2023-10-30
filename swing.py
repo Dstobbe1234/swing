832856851fb2494aaf7971088f2cc8957056862c
David Stobbe <d.stobbe1@share.epsb.ca>
2023-10-30 13:30:55 -0600

    started world

--- swing.py
+++ swing.py
@@ -1,12 +1,34 @@
 import pygame
 import math
+import random
 clock = pygame.time.Clock()
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screenSize = [1280, 720]
+screen = pygame.display.set_mode((screenSize[0], screenSize[1]))
 clock = pygame.time.Clock()
 running = True
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() /2)
 mouse = False
+boost = 1
+
+import matplotlib.pyplot as plt
+from perlin_noise import PerlinNoise
+
+
+def generateWorld():
+    noise = PerlinNoise(octaves=10, seed=1)
+    xpix = 64
+    ypix = 36
+    offsetX = random.randint(0, 5000)
+    offsetY = random.randint(0, 5000)
+    map = [[noise([i/xpix + offsetX, j/ypix + offsetY]) for j in range(xpix)] for i in range(ypix)]
+    for row in range(len(map)):
+        for i in range(len(map[row])):
+            if(map[row][i] >= -0.1):
+                pygame.draw.rect(screen, "white", (row + 20, i + 20, 20, 20))
+            
+
+
 
 def swing(mousePos, radius, angle, angleSpeed):
     mouse = True
@@ -16,8 +38,8 @@ def swing(mousePos, radius, angle, angleSpeed):
         screen.fill("black")
         pygame.draw.line(screen, 'white', mousePos, player_pos, 1)
         swingPos = [player_pos.copy()]
-
-        bobAcceleration =  0.00001 * math.cos(angle)
+        global boost
+        bobAcceleration =  0.00001 * math.cos(angle) * boost
         angleSpeed += bobAcceleration
         angle += angleSpeed
 
@@ -31,8 +53,15 @@ def swing(mousePos, radius, angle, angleSpeed):
         ev = pygame.event.get()
         for event in ev:
             if(event.type == pygame.MOUSEBUTTONUP):
-                bobSpeedVectors = [swingPos[1][0] - swingPos[0][0], swingPos[1][1] - swingPos[0][1]]
-                mouse = False
+                if(event.button == 1):
+                    bobSpeedVectors = [swingPos[1][0] - swingPos[0][0], swingPos[1][1] - swingPos[0][1]]
+                    mouse = False
+                elif(event.button == 3):
+                    boost = 1
+                    #bad
+            elif(event.type == pygame.MOUSEBUTTONDOWN):
+                if(event.button == 3):
+                    boost = 3
 
     fall(bobSpeedVectors)
     
@@ -70,4 +99,9 @@ while running:
     clock.tick(1) 
 
 pygame.quit()
-        
+
+
+            
+
+
+    
